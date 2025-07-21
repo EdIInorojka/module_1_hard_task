64a34d3cd5dc25ed6efeadc1637ca7dd5d0dba8e
Xiao <107986489+EdIInorojka@users.noreply.github.com>
2025-07-21 16:55:01 +0500

    merge_channels done

--- fan_in/merge_channels.go
+++ fan_in/merge_channels.go
@@ -1,9 +1,28 @@
 package fan_in
 
+import "sync"
+
 // MergeChannels - принимает несколько каналов на вход и объединяет их в один
 // Fan-in и merge channels синонимы
 func MergeChannels(channels ...<-chan int) <-chan int {
-	// TODO: ваш код
+	var wg sync.WaitGroup
+	result := make(chan int)
 
-	panic("implement me")
+	for _, channel := range channels {
+		ch := channel
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for n := range ch {
+				result <- n
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	return result
 }

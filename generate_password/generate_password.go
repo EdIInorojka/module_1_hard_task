ad2133ad7d6dc63330c7c5fd10721f67d73cdcd3
Xiao <107986489+EdIInorojka@users.noreply.github.com>
2025-07-21 17:07:47 +0500

    generate_password done

--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,29 @@
 package generate_password
 
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+)
+
 // letters - список допустимых символов в пароле
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
+	if n <= 0 {
+		return "", errors.New("Длина пароля не может быть отрицательной или равна нулю")
+	}
 
-	panic("implement me")
+	password := make([]byte, n)
+	length := big.NewInt(int64(len(letters)))
+
+	for i := range password {
+		num, err := rand.Int(rand.Reader, length)
+		if err != nil {
+			return "", err
+		}
+		password[i] = letters[num.Int64()]
+	}
+
+	return string(password), nil
 }
